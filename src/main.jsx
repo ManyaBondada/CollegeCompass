83be0d14b350ad9610cbb30459535ae791a80416
manyabondada <manya.bondada@gmail.com>
2024-10-16 15:46:03 -0500

    next ui + tailwind config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {NextUIProvider} from '@nextui-org/react'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </StrictMode>,
 )
