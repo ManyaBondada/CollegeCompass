874d847d96380d6f173fbde92bd2f554ac2815db
syl15 <listeph15@gmail.com>
2024-10-23 09:33:01 -0500

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 
+import {Input} from "@nextui-org/react";
+
 const Home = () => {
     return (
         <>
+        <h1 className="">Home</h1>
         <h1>Welcome [Name]</h1>
         <p className=""> Let's get started</p>
         <p className=""> Where are you moving from? </p>
+        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+            <Input type="location" isClearable />
+        </div>
+        <p className=""> What are you interested in? </p>
+        
+        
         </>
      );
 }
