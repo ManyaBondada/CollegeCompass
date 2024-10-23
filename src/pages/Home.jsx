28dc4a5cb781e5d78a7986637d03e57be1c7b2ad
syl15 <listeph15@gmail.com>
2024-10-23 09:32:30 -0500

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 
 const Home = () => {
     return (
+        <>
         <h1>Welcome [Name]</h1>
-
+        <p className=""> Let's get started</p>
+        <p className=""> Where are you moving from? </p>
+        </>
      );
 }
  
