dde253340ebc743ef36f7f88c9635d24748e1568
syl15 <listeph15@gmail.com>
2024-11-08 20:06:50 -0600

    only show store results when input changes

--- src/pages/StoresNearMe.jsx
+++ src/pages/StoresNearMe.jsx
@@ -2,6 +2,11 @@ import { Button } from "@nextui-org/react";
 import { Link } from 'react-router-dom';
 import BackButton from "./BackButton";
 
+import { Input } from "@nextui-org/input";
+import { Slider } from "@nextui-org/slider";
+import { DateRangePicker } from "@nextui-org/date-picker";
+import { useState } from "react";
+
 const StoresNearMe = () => {
   // Array of store data
   const stores = [
@@ -10,39 +15,67 @@ const StoresNearMe = () => {
     { id: 3, name: "Kroger", address: "160 N Coit Rd, Richardson, TX 75080" }
   ];
 
+  const [zipCode, setZipCode] = useState("");
+
   return (
     <div>
       <BackButton/>
       <h1>Stores Near Me</h1>
       <br></br>
-      <h2>Stores in your area</h2>
-
-      {stores.map((store, index) => (
-        <Link key={index} to={`/find-stores/${store.id}`}>
-          <Button
+      <div>
+        <Input 
+          placeholder="Enter zip code" 
+          className="w-full" 
+          onChange={(e) => setZipCode(e.target.value)} />
+      </div>
+      <br />
+      <div>
+          <p className="font-bold">Radius</p>
+          <Slider
+            label="Distance (miles)"
+            step={1}
+            maxValue={50}
+            minValue={0}
+            defaultValue={10}
+            className="max-w-md"
             color="secondary"
-            style={{
-              height: '85px',
-              width: '350px',
-              backgroundColor: '#EADAFF', 
-              color: '#000',
-              borderRadius: '10px',
-              fontSize: '30px',
-              paddingRight: '150px',
-              paddingBottom: '15px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              justifyContent: 'center',
-              marginBottom: '15px',
-              marginTop: '20px'
-            }}
-          >
-            <span>{store.name}</span>
-            <span style={{ fontSize: '15px', marginTop: '5px' }}>{store.address}</span>
-          </Button>
-        </Link>
-      ))}
+          />
+      </div>
+      <br />
+
+      {zipCode && (
+        <div>
+          <p>Stores in your area</p>
+          {stores.map((store, index) => (
+          <Link key={index} to={`/find-stores/${store.id}`}>
+            <Button
+              color="secondary"
+              style={{
+                height: '85px',
+                width: '350px',
+                backgroundColor: '#EADAFF', 
+                color: '#000',
+                borderRadius: '10px',
+                fontSize: '30px',
+                paddingRight: '150px',
+                paddingBottom: '15px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                justifyContent: 'center',
+                marginBottom: '15px',
+                marginTop: '20px'
+              }}
+            >
+              <span>{store.name}</span>
+              <span style={{ fontSize: '15px', marginTop: '5px' }}>{store.address}</span>
+            </Button>
+          </Link>
+        ))}
+        </div>
+        
+      )}
+
     </div>
   );
 };
