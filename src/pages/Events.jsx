135ea847b2397c290f2084a0dbaa7b0855149b42
syl15 <listeph15@gmail.com>
2024-11-26 18:45:58 -0600

    fixing Find an event UI

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -49,79 +49,79 @@ const Events = () => {
     return (
       <>
       <BackButton/>
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col">
         <h1>Find an Event</h1>
-        <div className="flex flex-col justify-between h-[calc(100vh-80px)] p-4 space-y-5">
-        <div style={{ display: 'flex', alignItems: 'center', gap: '10px'}}>
-          <Input 
-            placeholder="Enter zip code" 
-            className="w-full" 
-            value={zipCode} 
-            onChange={(e) => {
-              setZipCode(e.target.value);
-            }} 
-          />
-      </div>
-          <div>
-            <p className="font-bold">Radius</p>
-            <Slider
-              label="Distance (miles)"
-              step={1}
-              maxValue={50}
-              minValue={0}
-              defaultValue={10}
-              className="max-w-md"
-              color="secondary"
+        <div className="flex flex-col justify-between space-y-10">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10px', paddingTop: '20px'}}>
+            <Input 
+              placeholder="Enter zip code" 
+              className="w-full" 
+              value={zipCode} 
+              onChange={(e) => {
+                setZipCode(e.target.value);
+              }} 
             />
-          </div>
-
-          <div>
-            <p className="font-bold">Date</p>
-            <DateRangePicker 
-              label="Choose a date range" className="max-w-xs" 
-              defaultValue={{
-                start: parseDate(formattedDate), 
-                end: parseDate(formattedDate) 
-              }}
-              
+        </div>
+            <div>
+              <p className="font-bold pb-2">Radius</p>
+              <Slider
+                label="Distance (miles)"
+                step={1}
+                maxValue={50}
+                minValue={0}
+                defaultValue={10}
+                className="max-w-md"
+                color="secondary"
               />
-          </div>
+            </div>
 
-          <div>
-            <p className="font-bold">Select a culture</p>
-            <div className="flex flex-wrap gap-2 mt-2">
-              {cultures.map((culture) => (
-                <Button
-                  key={culture.value}
-                  onClick={() => handleCultureToggle(culture.value)}
-                  style={{
-                    backgroundColor: selectedCultures.includes(culture.value)
-                      ? "#EADAFF"
-                      : "#f9f9f9",
-                    color: selectedCultures.includes(culture.value)
-                      ? "#000"
-                      : "#666",
-                    border: "1px solid #ccc",
-                    margin: "0 4px",
-                  }}
-                >
-                  {culture.label}
-                </Button>
-              ))}
+            <div>
+              <p className="font-bold pb-2">Date</p>
+              <DateRangePicker 
+                label="Choose a date range" className="max-w-xs" 
+                defaultValue={{
+                  start: parseDate(formattedDate), 
+                  end: parseDate(formattedDate) 
+                }}
+                
+                />
+            </div>
+
+            <div>
+              <p className="font-bold pb-2">Select a culture</p>
+              <div className="flex flex-wrap gap-2 mt-2">
+                {cultures.map((culture) => (
+                  <Button
+                    key={culture.value}
+                    onClick={() => handleCultureToggle(culture.value)}
+                    style={{
+                      backgroundColor: selectedCultures.includes(culture.value)
+                        ? "#EADAFF"
+                        : "#f9f9f9",
+                      color: selectedCultures.includes(culture.value)
+                        ? "#000"
+                        : "#666",
+                      border: "1px solid #ccc",
+                      margin: "0 4px",
+                    }}
+                  >
+                    {culture.label}
+                  </Button>
+                ))}
+              </div>
+            </div>
+
+            <div className="mt-auto flex justify-center">
+              <Button
+                auto
+                onClick={handleSubmit}
+                style={{ backgroundColor: "#EADAFF", color: "#000" }}
+              >
+                Submit
+              </Button>
             </div>
           </div>
-
-          <div className="mt-auto flex justify-left pb-20">
-            <Button
-              auto
-              onClick={handleSubmit}
-              style={{ backgroundColor: "#EADAFF", color: "#000" }}
-            >
-              Submit
-            </Button>
-          </div>
         </div>
-      </div>
       </>
     );
 }
