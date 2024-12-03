168dddd03562cbac88f726b53d90a418592423aa
manyabondada <manya.bondada@gmail.com>
2024-12-02 20:52:53 -0600

    removed grocery checklists + added documentation

--- src/pages/GroceryShopping.jsx
+++ src/pages/GroceryShopping.jsx
@@ -1,64 +1,68 @@
-import {CheckboxGroup, Checkbox, Button} from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import BackButton from "./BackButton";
 
 const GroceryShopping = () => {
-    return (
-      <>
-        <BackButton />
-        <h1 className="text-4xl font-bold">Grocery Shopping</h1>
-        <br></br>
-        <CheckboxGroup label="Steps to Complete" color="secondary">
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Checkbox value="make-grocery-list" />
-            <Button
-              auto
-              color="secondary"
-              as="a"
-              href="/home/grocery-shopping/make-grocery-list"
+  const subtasks = [
+    {
+      title: "Subtask 1: Make a Grocery List",
+      description: "Plan ahead and list down all the essentials you need 📝",
+      route: "/home/grocery-shopping/make-grocery-list",
+    },
+    {
+      title: "Subtask 2: Find Stores Near Me",
+      description: "Discover the best stores around for your grocery needs 🛒",
+      route: "/home/grocery-shopping/find-stores",
+    },
+    {
+      title: "Subtask 3: Schedule a Visit",
+      description: "Set aside some time that works best for you to go shopping ⏰",
+      route: "/home/grocery-shopping/schedule-visit",
+    },
+  ];
+
+  return (
+    <>
+      <BackButton />
+      <h1 className="text-4xl font-bold">Grocery Shopping</h1>
+      <br />
+      <h2>Love to cook? Let's get the ingredients! Feel free to complete these subtasks as many times as needed in any order.</h2>
+      <br/>
+      <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+        {subtasks.map((subtask, index) => (
+          <div
+            key={index}
+            style={{
+              padding: "15px",
+              border: "1px solid #ccc",
+              borderRadius: "8px",
+              backgroundColor: "#f9f9f9",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <p style={{ fontWeight: "bold" }}>{subtask.title}</p>
+            <p>{subtask.description}</p>
+            <br />
+            <div
               style={{
-                marginLeft: "8px",
-                backgroundColor: "#EADAFF",
-                color: "#000",
+                display: "flex",
+                justifyContent: "center",
+                width: "100%",
               }}
             >
-              Make Grocery List
-            </Button>
+              <Button
+                as="a"
+                href={subtask.route}
+                style={{ backgroundColor: "#EADAFF", color: "#000" }}
+              >
+                Start
+              </Button>
+            </div>
           </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Checkbox value="find-stores" />
-            <Button
-              auto
-              color="secondary"
-              as="a"
-              href="/home/grocery-shopping/find-stores"
-              style={{
-                marginLeft: "8px",
-                backgroundColor: "#EADAFF",
-                color: "#000",
-              }}
-            >
-              Find Stores Near Me
-            </Button>
-          </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Checkbox value="schedule-visit" />
-            <Button
-              auto
-              color="secondary"
-              as="a"
-              href="/home/grocery-shopping/schedule-visit"
-              style={{
-                marginLeft: "8px",
-                backgroundColor: "#EADAFF",
-                color: "#000",
-              }}
-            >
-              Schedule Visit
-            </Button>
-          </div>
-        </CheckboxGroup>
-      </>
-    );
-}
- 
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default GroceryShopping;
