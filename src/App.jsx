7a3fa8dd09c32cc81fa647eca70194e59f896bf1
charliealpert <charlieyalpert@gmail.com>
2024-12-04 09:46:36 -0600

    fixed transparent nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -25,45 +25,72 @@ function App() {
   return (
     <>
       <Router>
-      <div className="flex flex-col min-h-screen overflow-x-hidden">
-        {hasCompletedOnboarding && (
-            <nav className="fixed bottom-0 left-0 w-full max-w-screen overflow-x-hidden p-4 border-t border-gray-300 z-10">
+        <div className="flex flex-col min-h-screen overflow-x-hidden">
+          {hasCompletedOnboarding && (
+            <nav className="fixed bottom-0 left-0 w-full max-w-screen overflow-x-hidden p-4 border-t border-gray-300 z-10 bg-white">
               <ul className="flex justify-between w-full max-w-screen-lg mx-auto z-10">
                 <li>
-                  <Link to="/home" className="">Home</Link>
+                  <Link to="/home" className="">
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/events" className="">Events</Link>
+                  <Link to="/events" className="">
+                    Events
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/settings" className="">Settings</Link>
+                  <Link to="/settings" className="">
+                    Settings
+                  </Link>
                 </li>
               </ul>
             </nav>
           )}
 
-        <div className="flex-grow p-4">
-          <Routes>
-            <Route path="/" element={
-                  hasCompletedOnboarding ? <Navigate to="/home" /> : <Onboarding setHasCompletedOnboarding={setHasCompletedOnboarding}/>
-                } />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/events" element={<Events/>} />
-            <Route path="/events/events-near-me" element={<EventsNearMe/>} />
-            <Route path="/settings" element={<Settings/>} />
-            <Route path="/home/grocery-shopping" element={<GroceryShopping/>} />
-            <Route path="/home/grocery-shopping/make-grocery-list" element={<GroceryList/>} />
-            <Route path="/home/grocery-shopping/schedule-visit" element={<ScheduleVisit/>} />
-            <Route path="/home/grocery-shopping/find-stores" element={<StoresNearMe/>} />
-            <Route path="/find-stores/:storeId" element={<StoreDetails />} />
-            <Route path="/bus-schedule" element={<BusMap />} />
-           <Route path="/events/:eventId" element={<EventDetails />} /> 
-          </Routes>
+          <div className="flex-grow p-4">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  hasCompletedOnboarding ? (
+                    <Navigate to="/home" />
+                  ) : (
+                    <Onboarding
+                      setHasCompletedOnboarding={setHasCompletedOnboarding}
+                    />
+                  )
+                }
+              />
+              <Route path="/home" element={<Home />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/events/events-near-me" element={<EventsNearMe />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route
+                path="/home/grocery-shopping"
+                element={<GroceryShopping />}
+              />
+              <Route
+                path="/home/grocery-shopping/make-grocery-list"
+                element={<GroceryList />}
+              />
+              <Route
+                path="/home/grocery-shopping/schedule-visit"
+                element={<ScheduleVisit />}
+              />
+              <Route
+                path="/home/grocery-shopping/find-stores"
+                element={<StoresNearMe />}
+              />
+              <Route path="/find-stores/:storeId" element={<StoreDetails />} />
+              <Route path="/bus-schedule" element={<BusMap />} />
+              <Route path="/events/:eventId" element={<EventDetails />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App
